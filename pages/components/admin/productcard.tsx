2b51d9e7677dc17c5b7271b629d756e42b54e4e6
Anton <62949848+icepaq@users.noreply.github.com>
2022-08-20 16:14:28 -0400

    fix undefined in productcard

--- pages/components/admin/productcard.tsx
+++ pages/components/admin/productcard.tsx
@@ -6,38 +6,38 @@ import { useEffect, useState } from 'react'
 const ProductCard = ({ product }: any) => {
 
     return (
-        <Link href={`/admin/product/${product._id}`}>
+        <Link href={`/admin/product/${product?._id || 'error'}`}>
         <div className={styles.productCard}>
             <div className={styles.productCardImageWrapper}>
-                <NextImage className={styles.productCardImage} src={product.imgur || '/diorsauvage.jpg'} width={70} height={160} />
+                <NextImage className={styles.productCardImage} src={product?.imgur || '/diorsauvage.jpg'} width={70} height={160} />
             </div>
             <div className={styles.productCardInfo}>
                 <div className={styles.productCardInfoRow}>
                     <div className={styles.productCardInfoBox}>
                         <div className={styles.productCardInfoBoxTitle}>Brand</div>
-                        <div className={styles.productCardInfoBoxText}>{product.brand}</div>
+                        <div className={styles.productCardInfoBoxText}>{product?.brand}</div>
                     </div>
                     <div className={styles.productCardInfoBox}>
                         <div className={styles.productCardInfoBoxTitle}>Product name</div>
-                        <div className={styles.productCardInfoBoxText}>{product.name}</div>
+                        <div className={styles.productCardInfoBoxText}>{product?.name}</div>
                     </div>
                     <div className={styles.productCardInfoBox}>
                         <div className={styles.productCardInfoBoxTitle}>Product ID</div>
-                        <div className={styles.productCardInfoBoxText}>{product._id}</div>
+                        <div className={styles.productCardInfoBoxText}>{product?._id}</div>
                     </div>
                 </div>
                 <div className={styles.productCardInfoRow}>
                     <div className={styles.productCardInfoBox}>
                         <div className={styles.productCardInfoBoxTitle}>Gender</div>
-                        <div className={styles.productCardInfoBoxText}>{product.male}</div>
+                        <div className={styles.productCardInfoBoxText}>{product?.male}</div>
                     </div>
                     <div className={styles.productCardInfoBox}>
                         <div className={styles.productCardInfoBoxTitle}>Price / Month</div>
-                        <div className={styles.productCardInfoBoxText}>{product.monthly_price}</div>
+                        <div className={styles.productCardInfoBoxText}>{product?.monthly_price}</div>
                     </div>
                     <div className={styles.productCardInfoBox}>
                         <div className={styles.productCardInfoBoxTitle}>Unit Price</div>
-                        <div className={styles.productCardInfoBoxText}>{product.unit_price}</div>
+                        <div className={styles.productCardInfoBoxText}>{product?.unit_price}</div>
                     </div>
                 </div>
             </div>
