570a5888b4042618a86bc50517ca50e51c0d6b47
Anton <62949848+icepaq@users.noreply.github.com>
2022-08-24 13:14:05 -0400

    add year and date to orders

--- pages/api/storerecommendations.ts
+++ pages/api/storerecommendations.ts
@@ -10,6 +10,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     
     const rawOrders = JSON.parse(req.body.products);
     const month = new Date().getMonth();
+    const date = new Date().getDate();
+    let year = new Date().getFullYear();
 
     const rawFutureOrders = JSON.parse(req.body.futureProducts);
 
@@ -18,10 +20,17 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const orders = [];
     orders.push({month: month, order: rawOrders});
 
+    let adjustedMonth = month;
     for(let i = 0; i < rawFutureOrders.length; i++) {
-        orders.push({month: month + i + 1, order: rawFutureOrders[i]});
-    }
+        if (adjustedMonth > 11) {
+            year++;
+            adjustedMonth = adjustedMonth - 11;
+        } else {
+            adjustedMonth = adjustedMonth + 1;
+        }
 
+        orders.push({month: adjustedMonth + 1, date: date, year: year, order: rawFutureOrders[i]});
+    }
     
 
     await collection.insertOne({email: req.body.email, orders: orders});
